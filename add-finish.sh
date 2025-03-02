5a8b0914b519a1a8c0ff68565dcb5232a6be572a
ThePolishCat <damianadam000@gmail.com>
2025-03-02 21:32:56 +0100

    db fix

--- add-finish.sh
+++ add-finish.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+rm -fr x86_64/catlinux.db*
+rm -fr x86_64/catlinux.files*
+
+
 repo-add --sign --verify --key E40E7EDDF0FF9143 x86_64/catlinux.db.tar.gz x86_64/*.pkg.tar.zst
 
 rm -fr x86_64/catlinux.db
