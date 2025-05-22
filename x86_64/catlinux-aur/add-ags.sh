087f870527dccfdd8b3fd5aa164f1008826e479a
ThePolishCat <damianadam000@gmail.com>
2025-05-22 12:38:44 +0200

    update repo

--- x86_64/catlinux-aur/add-ags.sh
+++ x86_64/catlinux-aur/add-ags.sh
@@ -2,4 +2,4 @@
 
 PKGS="aylurs-gtk-shell-git libastal-meta libastal-gjs-git libastal-git libastal-4-git libastal-auth-git libastal-apps-git libastal-battery-git libastal-bluetooth-git libastal-cava-git libastal-greetd-git libastal-hyprland-git libastal-io-git libastal-mpris-git libastal-network-git libastal-notifd-git libastal-powerprofiles-git libastal-river-git libastal-tray-git libastal-wireplumber-git "
 
-GPGKEY="E40E7EDDF0FF9143" PACKAGER="ThePolishCat <damianadam000@gmail.com>" aur sync --repo catlinux --root . --noview --sign --rebuild --clean $PKGS
+GPGKEY="E40E7EDDF0FF9143" aur sync --repo catlinux --root . --noview --sign --rebuild --clean $PKGS
